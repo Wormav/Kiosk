13cb0c0e3f2497a0a032f3f29a45a9bc9b9742fa
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-12-25 21:42:38 +0100

    Add suppressHydrationWarning to html and body

--- app/root.tsx
+++ app/root.tsx
@@ -51,14 +51,14 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export const Layout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="fr">
+  <html lang="fr" suppressHydrationWarning>
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Meta />
       <Links />
     </head>
-    <body>
+    <body suppressHydrationWarning>
       <MantineProvider theme={theme} forceColorScheme="light">
         {children}
       </MantineProvider>
